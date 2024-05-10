123939f925686b5ec5aa3f94ba55c3eb25ff7f37
Fanil <fanilfan1994@mail.ru>
2024-05-10 15:23:26 +0300

    first commit

---
+++ src/components/lib/button/BaseButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button" :class="classes">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonProps } from "@/components/types/types";
+import { computed } from "vue";
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: "primary",
+  size: "normal"
+});
+
+const classes = computed(() => {
+  return `test-lib-${props.type}`;
+});
+</script>
+
+<style lang="scss" scoped>
+.button {
+  background-color: #0ebb11;
+}
+</style>
---
+++ src/examples/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import BaseButton from "@/components/lib/button/BaseButton.vue";
+import RadioGroup from "@/components/lib/radio/RadioGroup.vue";
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <BaseButton> Test </BaseButton>
+      <RadioGroup></RadioGroup>
+    </div>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
